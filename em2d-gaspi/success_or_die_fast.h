0597f761c612cf454502aa917a6336f373a40461
BrunoAmorim97 <97bruno.amorim@gmail.com>
2020-08-09 21:49:34 +0100

    Improved success or die macros

--- em2d-gaspi/success_or_die_fast.h
+++ em2d-gaspi/success_or_die_fast.h
@@ -5,7 +5,28 @@
 #include <stdlib.h>
 
 
-#define SUCCESS_OR_DIE(...) if ( __VA_ARGS__ != GASPI_SUCCESS) exit (EXIT_FAILURE);
+#define SUCCESS_OR_DIE(...)								\
+	do													\
+	{													\
+		const gaspi_return_t r = __VA_ARGS__;			\
+														\
+		if (r != GASPI_SUCCESS)							\
+		{												\
+			exit (EXIT_FAILURE);						\
+		}												\
+	} while (0)
+
+
+#define SUCCESS_TIMEOUT_OR_DIE(...)						\
+	do													\
+	{													\
+		const gaspi_return_t r = __VA_ARGS__;			\
+														\
+		if (r != GASPI_SUCCESS && r != GASPI_TIMEOUT)	\
+		{												\
+			exit (EXIT_FAILURE);						\
+		}												\
+	} while (0)
 
 
 #endif
